c3388d231e6afc197f30dfb57b524f5f7700dbee
Sebastian Bolanos <sebasheston@protonmail.com>
2022-06-16 13:13:23 +0100

    Init flake for now

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "Rust broker-v2 dev environment";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    deploy-rs.url = "github:serokell/deploy-rs";
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -11,13 +12,19 @@
       url = "github:numtide/flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    naersk = {
+      url = "github:nmattia/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
     fenix,
+    naersk,
     flake-utils,
+    deploy-rs,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (
@@ -28,36 +35,81 @@
             fenix.overlay
           ];
         };
-        nativeBuildInputs = with pkgs; [
+        nativeBuildInputs =
+          [
             (fenix.packages."${system}".stable.withComponents [
               "cargo"
               "rust-src"
               "rust-docs"
               "rustc"
             ])
-            cargo-watch
-            rust-analyzer
-            pkg-config
-            glib
-            openssl
-          ] ++ lib.optionals stdenv.isDarwin [
+            pkgs.cargo-watch
+            pkgs.rust-analyzer
+            pkgs.pkg-config
+            pkgs.glib
+            pkgs.openssl
+          ]
+          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
             pkgs.darwin.apple_sdk.frameworks.Security
             pkgs.darwin.apple_sdk.frameworks.CoreFoundation
           ];
-
       in {
         devShells.default = pkgs.mkShell {
           inherit nativeBuildInputs;
           RUST_SRC_PATH = "${fenix.packages.${system}.stable.rust-src}/bin/rust-lib/src";
           LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath nativeBuildInputs;
-          NIX_LDFLAGS="${pkgs.lib.optionalString pkgs.stdenv.isDarwin "\
+          NIX_LDFLAGS = "${pkgs.lib.optionalString pkgs.stdenv.isDarwin "\
             -F${pkgs.darwin.apple_sdk.frameworks.Security}/Library/Frameworks -framework Security \
             -F${pkgs.darwin.apple_sdk.frameworks.CoreFoundation}/Library/Frameworks -framework CoreFoundation"}";
           buildInputs = with pkgs; [
-            (fenix.packages."${system}".stable.withComponents [ "clippy" "rustfmt"])
-            just
+            (fenix.packages."${system}".stable.withComponents ["clippy" "rustfmt"])
+            pkgs.just
+            deploy-rs.packages.x86_64-linux.deploy-rs
           ];
         };
+        nixosConfigurations = {
+          "keys.walletconnect.com" = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+              ({pkgs, ...}: {
+                nix = {
+                  extraOptions = ''
+                    experimental-features = nix-command flakes
+                    keep-outputs = true
+                    keep-derivations = true
+                  '';
+                  trustedUsers = ["root"];
+                  gc = {
+                    automatic = true;
+                    options = "--delete-older-than 10d";
+                  };
+                };
+              })
+              ({config, ...}: {
+                networking.hostName = "keys-walletconnect-com";
+              })
+              ./hosts/keys.walletconnect.com
+            ];
+          };
+        };
+
+        deploy.nodes = {
+          "keys.walletconnect.com" = {
+            hostname = "159.65.123.131";
+            sshUser = "root";
+            fastConnection = true;
+            profiles.system = {
+              user = "root";
+              path =
+                deploy-rs.lib.x86_64-linux.activate.nixos
+                self.nixosConfigurations."keys.walletconnect.com";
+            };
+          };
+        };
+        checks =
+          builtins.mapAttrs
+          (system: deployLib: deployLib.deployChecks self.deploy)
+          deploy-rs.lib;
       }
     );
 }
