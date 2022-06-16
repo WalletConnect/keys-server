e7bc53892f0cc445a5eb3763b966f5594fcc7c18
szymon <sz.rzad@gmail.com>
2022-06-16 16:51:02 +0200

    Added GithubAction PubKey

--- hosts/keys.walletconnect.com/default.nix
+++ hosts/keys.walletconnect.com/default.nix
@@ -12,5 +12,6 @@
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKVet4QtQ6sqKJKBHIO4cZ1sgLSFSoJBL9Ub4JnpnplP github.com/Elyniss.keys"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOfN08jC7Rkmnbk2wE1UVuLUalQQU+yYi2017RZ7OcBD sebas@mini"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE9642iDnf+zFOhyJABZtFALE+mC7FNRRzzXJ3B5J90O github_action"
   ];
 }
