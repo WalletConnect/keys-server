1bedbacb01c8f1e2d4b3d5e5d23d2f1ce6236ce7
Derek <alexanderderekrein@gmail.com>
2022-11-22 11:39:53 -0500

    feat(infra): spin up service

---
+++ terraform/ecs/variables.tf
@@ -0,0 +1,35 @@
+variable "ecr_repository_url" {
+  type = string
+}
+
+variable "app_name" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "port" {
+  type = number
+}
+
+variable "acm_certificate_arn" {
+  type = string
+}
+
+variable "fqdn" {
+  type = string
+}
+
+variable "route53_zone_id" {
+  type = string
+}
+
+variable "prometheus_endpoint" {
+  type = string
+}
