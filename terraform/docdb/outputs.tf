d142772be2437183c841105de3b451ce7ce50b43
szymon <sz.rzad@gmail.com>
2023-01-23 19:39:19 +0100

    Revert "fix: Remove nonexisting cert (#42)"
    This reverts commit 85bf66fe00f91c0e8af6e3c8053127246bb3ab4c.

--- terraform/docdb/outputs.tf
+++ terraform/docdb/outputs.tf
@@ -15,7 +15,7 @@ output "port" {
 }
 
 output "connection_url" {
-  value = "mongodb://${aws_docdb_cluster.docdb_primary.master_username}:${aws_docdb_cluster.docdb_primary.master_password}@${aws_docdb_cluster.docdb_primary.endpoint}:${aws_docdb_cluster.docdb_primary.port}/${var.default_database}?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false&minPoolSize=32&maxPoolSize=256&maxIdleTimeMS=30000&connectTimeoutMS=30000"
+  value = "mongodb://${aws_docdb_cluster.docdb_primary.master_username}:${aws_docdb_cluster.docdb_primary.master_password}@${aws_docdb_cluster.docdb_primary.endpoint}:${aws_docdb_cluster.docdb_primary.port}/${var.default_database}?tls=true&tlsCaFile=rds-combined-ca-bundle.pem&tlsAllowInvalidCertificates=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false&minPoolSize=32&maxPoolSize=256&maxIdleTimeMS=30000&connectTimeoutMS=30000"
 }
 
 output "cluster_id" {
