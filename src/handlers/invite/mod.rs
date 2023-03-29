02f375470012fcf5e6965ed2cdf2a064ca7aa2a5
Darksome <dev@darkso.me>
2023-03-29 15:32:39 +0400

    Fix iss->sub & add some docs

--- src/handlers/invite/mod.rs
+++ src/handlers/invite/mod.rs
@@ -15,8 +15,17 @@ pub struct InviteKeyClaims {
     aud: String, // keys server url used for registering
     exp: usize,  // timestamp when jwt must expire TODO: Should be 1 hour
     iat: usize,  // timestamp when jwt was issued
-    iss: String, // public identity key in form of did:key, also used to verify jwt signature
-    sub: String, // public key for chat invite key
+
+    /// Public identity key in form of did:key according to the [Ed25519][1]
+    ///
+    /// [1]: https://w3c-ccg.github.io/did-method-key/#ed25519-x25519
+    iss: String,
+
+    /// Public key for chat invite key in form of did:key according to the [X25519][1]
+    ///
+    /// [1]: https://w3c-ccg.github.io/did-method-key/#x25519
+    sub: String,
+
     pkh: String, // corresponding blockchain account (did:pkh)
 }
 
@@ -26,10 +35,10 @@ impl JwtClaims for InviteKeyClaims {
         // aud must be equal this dns?
         // exp must be in future
         // iat must be in past
-        // sub must be valid public key
+        // iss must be valid did:key
         // pkh must be valid did:pkh
 
-        did::validate_x25519(&self.iss)
+        did::validate_x25519(&self.sub)
     }
 }
 
@@ -59,13 +68,13 @@ mod test_claims_validation {
         let mut claims = default();
         assert!(!claims.is_valid());
 
-        claims.iss = "ababagalamaga".to_string();
+        claims.sub = "ababagalamaga".to_string();
         assert!(!claims.is_valid());
 
-        claims.iss = "did:key:zQ3shokFTS3brHcDQrn82RUDfCZESWL1ZdCEJwekUDPQiYBme".to_string();
+        claims.sub = "did:key:zQ3shokFTS3brHcDQrn82RUDfCZESWL1ZdCEJwekUDPQiYBme".to_string();
         assert!(!claims.is_valid());
 
-        claims.iss = "did:abc".to_string();
+        claims.sub = "did:abc".to_string();
         assert!(!claims.is_valid());
     }
 
@@ -73,13 +82,13 @@ mod test_claims_validation {
     fn succeeds_on_correct_claims() {
         let mut claims = default();
 
-        claims.iss = "did:key:z6LSeu9HkTHSfLLeUs2nnzUSNedgDUevfNQgQjQC23ZCit6F".to_string();
+        claims.sub = "did:key:z6LSeu9HkTHSfLLeUs2nnzUSNedgDUevfNQgQjQC23ZCit6F".to_string();
         assert!(claims.is_valid());
 
-        claims.iss = "did:key:z6LStiZsmxiK4odS4Sb6JmdRFuJ6e1SYP157gtiCyJKfrYha".to_string();
+        claims.sub = "did:key:z6LStiZsmxiK4odS4Sb6JmdRFuJ6e1SYP157gtiCyJKfrYha".to_string();
         assert!(claims.is_valid());
 
-        claims.iss = "did:key:z6LSoMdmJz2Djah2P4L9taDmtqeJ6wwd2HhKZvNToBmvaczQ".to_string();
+        claims.sub = "did:key:z6LSoMdmJz2Djah2P4L9taDmtqeJ6wwd2HhKZvNToBmvaczQ".to_string();
         assert!(claims.is_valid());
     }
 }
