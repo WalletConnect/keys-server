61e71ea0aa5d7f6b956b8a12e6934160efbcb39f
Xavier Basty <xavier@walletconnect.com>
2023-08-14 15:16:44 +0200

    fix: remove old infra and CI

--- terraform/monitoring/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "prometheus_url" {
-  description = "The URL of the Prometheus server to use for this dashboard."
-  value       = local.prometheus_url
-}
-
-output "dashboard_definition" {
-  description = "The JSON definition of the dashboard."
-  value       = data.jsonnet_file.dashboard.rendered
-}
