ee842af2bf27d17b6e4232e8a9e87e5c9f89a21c
daniel <daniel@nopestck.dev>
2024-10-09 10:35:29 +0200

    fmt HCL

--- terraform/ecs/cluster.tf
+++ terraform/ecs/cluster.tf
@@ -7,9 +7,9 @@ locals {
   task_memory = module.this.stage == "prod" ? var.task_memory : 512
 
   otel_image_tag = "v0.3.0"
-  otel_port   = var.port + 1
-  otel_cpu    = 128
-  otel_memory = 128
+  otel_port      = var.port + 1
+  otel_cpu       = 128
+  otel_memory    = 128
 }
 
 module "ecs_cpu_mem" {
